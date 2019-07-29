c9b42d96aa122d0828f0a06e0fd1a48618872244
alamasrorul <alamasrorulhaqqracer@gmail.com>
2019-07-30 02:48:41 +0700

    commit fix

---
+++ app/src/main/java/com/example/ululmusthofa/admin/AdminLagu/ListLagu/tampilan/Main2Activity.java
@@ -0,0 +1,191 @@
+package com.example.ululmusthofa.admin.AdminLagu.ListLagu.tampilan;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ululmusthofa.R;
+import com.example.ululmusthofa.admin.AdminLagu.ListLagu.EditListLagu;
+import com.example.ululmusthofa.admin.AdminLagu.ListLagu.PojoLagu;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.concurrent.TimeUnit;
+
+import java.util.logging.LogRecord;
+
+
+public class Main2Activity extends AppCompatActivity implements MediaPlayer.OnBufferingUpdateListener,MediaPlayer.OnCompletionListener{
+
+    private ImageButton btn_play_pause;
+    private SeekBar seekBar;
+    private TextView textView;
+    private WebView webview;
+
+
+
+    private MediaPlayer mediaPlayer;
+    private int mediaFileLength;
+    private int realtimeLength;
+    private WebView webView;
+   final Handler handler = new Handler() ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        final PojoLagu barang = (PojoLagu) getIntent().getSerializableExtra("date");
+
+
+        //musicView = (VusikView)findViewById(R.id.musicView);
+
+        //webview
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+
+        String url="";
+        try {
+            url= URLEncoder.encode(barang.getPdfUrl(),"UTF-8"); //Url Convert to UTF-8 It important.
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        webView.loadUrl("https://docs.google.com/gview?embedded=true&url="+url);
+
+        //set seek bar
+        seekBar = (SeekBar)findViewById(R.id.seekbar);
+        seekBar.setMax(99); // 100% (0~99)
+        seekBar.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(mediaPlayer.isPlaying())
+                {
+                    SeekBar seekBar = (SeekBar)v;
+                    int playPosition = (mediaFileLength/100)*seekBar.getProgress();
+                    mediaPlayer.seekTo(playPosition);
+                }
+                return false;
+            }
+        });
+
+        //set timer dan button play
+        textView = (TextView)findViewById(R.id.textTimer);
+
+        btn_play_pause = (ImageButton) findViewById(R.id.btn_play_pause);
+        btn_play_pause.setOnClickListener(new View.OnClickListener() {
+
+
+
+
+            @Override
+            public void onClick(View v) {
+                final ProgressDialog mDialog = new ProgressDialog(Main2Activity.this);
+
+
+                AsyncTask<String,String,String> mp3Play = new AsyncTask<String, String, String>() {
+
+                    @Override
+                    protected void onPreExecute() {
+                        mDialog.setMessage("Please wait");
+                        mDialog.show();
+                    }
+
+                    @Override
+                    protected String doInBackground(String... params) {
+                        try{
+                            mediaPlayer.setDataSource(params[0]);
+                            mediaPlayer.prepare();
+                        }
+                        catch (Exception ex)
+                        {
+
+                        }
+                        return "";
+                    }
+
+                    @Override
+                    protected void onPostExecute(String s) {
+                        mediaFileLength = mediaPlayer.getDuration();
+                        realtimeLength = mediaFileLength;
+                        if(!mediaPlayer.isPlaying())
+                        {
+                            mediaPlayer.start();
+                            btn_play_pause.setImageResource(R.drawable.ic_pause);
+                        }
+                        else
+                        {
+                            mediaPlayer.pause();
+                            btn_play_pause.setImageResource(R.drawable.ic_play);
+                        }
+
+                        updateSeekBar();
+                        mDialog.dismiss();
+                    }
+                };
+
+                mp3Play.execute(barang.getMp3Url()); // direct link mp3 file
+
+
+            }
+        });
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setOnBufferingUpdateListener(this);
+        mediaPlayer.setOnCompletionListener(this);
+
+
+    }
+
+    private void updateSeekBar() {
+        seekBar.setProgress((int)(((float)mediaPlayer.getCurrentPosition() / mediaFileLength)*100));
+        if(mediaPlayer.isPlaying())
+        {
+            Runnable updater = new Runnable() {
+                @Override
+                public void run() {
+                    updateSeekBar();
+                    realtimeLength-=1000; // declare 1 second
+                    textView.setText(String.format("%d:%d",TimeUnit.MILLISECONDS.toMinutes(realtimeLength),
+                            TimeUnit.MILLISECONDS.toSeconds(realtimeLength) -
+                                    TimeUnit.MILLISECONDS.toSeconds(TimeUnit.MILLISECONDS.toMinutes(realtimeLength))));
+
+                }
+
+            };
+            handler.postDelayed(updater,1000); // 1 second
+        }
+    }
+
+    @Override
+    public void onBufferingUpdate(MediaPlayer mp, int percent) {
+        seekBar.setSecondaryProgress(percent);
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        btn_play_pause.setImageResource(R.drawable.ic_play);
+
+
+    }
+
+    public static Intent getActIntent(Activity activity) {
+        return new Intent(activity, Main2Activity.class);
+    }
+
+}
---
+++ app/src/main/java/com/example/ululmusthofa/admin/Login/PojoAdmin.java
@@ -0,0 +1,15 @@
+package com.example.ululmusthofa.admin.Login;
+
+public class PojoAdmin {
+
+    public PojoAdmin(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String username;
+    public String email;
+
+    public PojoAdmin() {
+    }
+}
